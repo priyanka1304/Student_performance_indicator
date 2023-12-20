f0dc70140c81b648dc07ad69bbf20ae800e80e9f
priyanka1304 <priyankasingh131313@gmail.com>
2023-12-20 17:57:04 +0530

    logging and exception handling

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup,find_packages
+from typing import List
+
+HYPHEN_E_DOT = '-e .'
+def get_requirements(file_path)->List[str]:
+    '''
+    This function will return a list of requirements
+    '''
+    requirements=[]
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace('\n','') for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+
+
+
+setup(
+    name='Student performance analysis',
+    version='0.0.1',
+    author='Priyanka Singh',
+    author_email='priyankasingh131313@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
